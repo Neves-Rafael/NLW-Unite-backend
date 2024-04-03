63f9faae750b599e54332115ee443a2f76fc7841
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-04-03 19:44:17 -0300

    :sparkles: Feat table attendees

---
+++ prisma/migrations/20240403224317_create_table_attendees/migration.sql
@@ -0,0 +1,31 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `maximun_attendees` on the `events` table. All the data in the column will be lost.
+
+*/
+-- CreateTable
+CREATE TABLE "attendees" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "event_id" TEXT NOT NULL,
+    CONSTRAINT "attendees_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "events" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_events" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "details" TEXT,
+    "slug" TEXT NOT NULL,
+    "maximum_attendees" INTEGER
+);
+INSERT INTO "new_events" ("details", "id", "slug", "title") SELECT "details", "id", "slug", "title" FROM "events";
+DROP TABLE "events";
+ALTER TABLE "new_events" RENAME TO "events";
+CREATE UNIQUE INDEX "events_slug_key" ON "events"("slug");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
