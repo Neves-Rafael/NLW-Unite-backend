4218f38ebbce3536eb8e3d97a0e91b4d5d6b01e4
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-04-04 14:02:05 -0300

    :sparkles: Feat check ins table

--- src/routes/get-attendee-badge.ts
+++ src/routes/get-attendee-badge.ts
@@ -12,7 +12,14 @@ export async function getAttendeeBadge(app: FastifyInstance){
           attendeeId: z.coerce.number().int(),
         }),
         response:{
-
+            200: z.object({
+              badge: z.object({
+                name: z.string(),
+                email: z.string().email(),
+                eventTitle: z.string(),
+                checkInURL: z.string().url(),
+              })
+            })
         },
       }
     }, async (request, reply) => {
@@ -37,8 +44,17 @@ export async function getAttendeeBadge(app: FastifyInstance){
         throw new Error("Attendee not found.")
       }
 
+      const baseURL = `${request.protocol}://${request.hostname}`;
+
+      const checkInURL = new URL(`/attendees/${attendeeId}/check-in`, baseURL)
+
       return reply.send({
-        attendee
+        badge:{
+          name: attendee.name,
+          email: attendee.email,
+          eventTitle: attendee.event.title,
+          checkInURL: checkInURL.toString(),
+        }
       })
     })
 }
