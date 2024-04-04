e13dc270538565351373565be934bffee3336ded
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-04-03 21:25:10 -0300

    :sparkles: Feat get-attendee-bagde route

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { serializerCompiler, validatorCompiler} from "fastify-type-provider-zod"
 import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
 import { getEvent } from "./routes/get-event";
+import { getAttendeeBadge } from "./routes/get-attendee-badge";
 
 const app = fastify();
 
@@ -12,7 +13,7 @@ app.setSerializerCompiler(serializerCompiler);
 app.register(createEvent);
 app.register(registerForEvent);
 app.register(getEvent);
-
+app.register(getAttendeeBadge)
 app.listen({ port: 3333}).then(() => {
   console.log("HTTP server running!")
 })
