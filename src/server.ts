52a399ffb00867d2a08c180a638f0308122d7dcc
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-04-04 14:15:34 -0300

    :sparkles: Feat check in route

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
 import { getEvent } from "./routes/get-event";
 import { getAttendeeBadge } from "./routes/get-attendee-badge";
+import { checkIn } from "./routes/check-in";
 
 const app = fastify();
 
@@ -13,7 +14,11 @@ app.setSerializerCompiler(serializerCompiler);
 app.register(createEvent);
 app.register(registerForEvent);
 app.register(getEvent);
-app.register(getAttendeeBadge)
+app.register(getAttendeeBadge);
+app.register(checkIn);
+
+
+
 app.listen({ port: 3333}).then(() => {
   console.log("HTTP server running!")
 })
