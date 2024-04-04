d2b95c82d40fc0ff12f6513c9a2e398fb278269e
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-04-04 16:31:53 -0300

    :sparkles: Feat event-attendees route

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import { registerForEvent } from "./routes/register-for-event";
 import { getEvent } from "./routes/get-event";
 import { getAttendeeBadge } from "./routes/get-attendee-badge";
 import { checkIn } from "./routes/check-in";
+import { getEventAttendees } from "./routes/get-event-attendees";
 
 const app = fastify();
 
@@ -16,6 +17,7 @@ app.register(registerForEvent);
 app.register(getEvent);
 app.register(getAttendeeBadge);
 app.register(checkIn);
+app.register(getEventAttendees);
 
 
 
